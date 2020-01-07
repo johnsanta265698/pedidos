ea288755979dfe205356a4e83544c1d433bd4532
John Santa <johnsanta265698@users.noreply.github.com>
2020-01-06 20:53:31 -0500

    Se genera componente pedidos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PedidosComponent } from './pedidos/pedidos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PedidosComponent
   ],
   imports: [
     BrowserModule,
