55d0be276e83a1601839509c5ceae5153c40c3c6
John Santa <johnsanta265698@users.noreply.github.com>
2020-01-07 16:45:10 -0500

    Se crea logica de servicio clientes

--- src/app/services/clientes.service.ts
+++ src/app/services/clientes.service.ts
@@ -2,6 +2,7 @@
  * Este servicio sera usado para guardar nuestros clientes en LocalStorage
  */
 import { Injectable } from '@angular/core';
+import { Cliente } from '../models/clientes';
 
 @Injectable({
   providedIn: 'root'
@@ -9,4 +10,23 @@ import { Injectable } from '@angular/core';
 export class ClientesService {
 
   constructor() { }
+
+  agregarLocalStorage(cliente: Cliente){
+
+    let clientesAntiguos: Cliente[] = this.clientesLocalStorage
+    cliente.clienteID = clientesAntiguos.length + 1
+
+    clientesAntiguos.push(cliente)
+
+    localStorage.setItem('clientes', JSON.stringify(clientesAntiguos))
+  }
+
+  get clientesLocalStorage() : Cliente[] {
+    let clientesDesdeLocalStorage: Cliente[] = JSON.parse(localStorage.getItem('clientes'))
+    if (clientesDesdeLocalStorage == null) {
+      return new Array<Cliente>()
+    } else {
+      return clientesDesdeLocalStorage
+    }
+  }
 }
