b9b319a31befd4fd651140b769edcb30beea5da3
John Santa <johnsanta265698@users.noreply.github.com>
2020-01-06 21:45:01 -0500

    Se crea logica y botones de guardar cliente y eliminar cliente

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Clientes {
+  nombre: string
+  apellido: string
+  edad: number
+}
+
 @Component({
   selector: 'app-clientes',
   templateUrl: './clientes.component.html',
   styleUrls: ['./clientes.component.scss']
 })
 export class ClientesComponent implements OnInit {
+  clientes: Array<Clientes> = new Array<Clientes>()
 
   constructor() { }
 
   ngOnInit() {
+    this.clientes.push(
+      {
+        nombre: 'John',
+        apellido: 'Santa',
+        edad: 23
+      },
+      {
+        nombre: 'Maicol',
+        apellido: 'Santa',
+        edad: 16
+      }
+    )
   }
 
+  guardarCliente() {
+    localStorage.setItem('clientes', JSON.stringify(this.clientes))
+  }
+
+  eliminarTodo(){
+    localStorage.clear()
+  }
 }
