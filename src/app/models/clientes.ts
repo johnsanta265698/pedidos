b80e53adbbf68d1f3c96533181db8917f68ae7d9
John Santa <johnsanta265698@users.noreply.github.com>
2020-01-07 16:45:33 -0500

    Se crea modelo de clientes

---
+++ src/app/models/clientes.ts
@@ -0,0 +1,16 @@
+/**
+ * Esta clase sera la que establezca los atributos con los cuales contara cada uno de nuestros clientes
+ */
+export class Cliente{
+    clienteID: number
+    nombre: string
+    apellido: string
+    direccion: string
+
+    constructor(){
+        this.clienteID = this.clienteID
+        this.nombre = this.nombre
+        this.apellido = this.apellido
+        this.direccion = this.direccion
+    }
+}
