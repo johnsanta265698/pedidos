ff2a784aa1ab36d9e47ae99362bebed4ec19e10d
John Santa <johnsanta265698@users.noreply.github.com>
2020-01-06 21:28:59 -0500

    Se reparan problemas en cuanto a merge anteriores

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,36 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { EncabezadoComponent } from './encabezado/encabezado.component';
-=======
 import { ClientesComponent } from './clientes/clientes.component';
->>>>>>> Clientes
-=======
 import { ProductosComponent } from './productos/productos.component';
->>>>>>> Productos
-=======
 import { PedidosComponent } from './pedidos/pedidos.component';
->>>>>>> Pedidos
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-    EncabezadoComponent
-=======
-    ClientesComponent
->>>>>>> Clientes
-=======
-    ProductosComponent
->>>>>>> Productos
-=======
-    PedidosComponent
->>>>>>> Pedidos
+    EncabezadoComponent,
+    ClientesComponent,
+    ProductosComponent,
+    PedidosComponent,
   ],
   imports: [
     BrowserModule,
